aec0fe9955d02e306090cf51f9a3beeedb313335
hadikp <hp20001025@gmail.com>
2021-10-26 22:35:28 +0200

    start projekt

---
+++ week03-lab-tasks/src/main/java/da03/GuessTheNumber.java
@@ -0,0 +1,10 @@
+package da03;
+
+import java.util.Scanner;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Kérek egy számot!");
+    }
+}
